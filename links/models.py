df5b0fdc4b72ca3d485f56c9ed86f82295c0719e
Phenomfunbi <62062746+phenomfunbi@users.noreply.github.com>
2022-07-17 22:01:21 +0800

    Add files via upload

---
+++ links/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model 
+user=get_user_model()
+
+# Create your models here.
+
+class Link(models.Model):
+    target_url = models.URLField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    identifier = models.SlugField(
+        max_length= 20, blank =True, unique = True
+    )
+    author = models.ForeignKey(user, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    active = models.BooleanField(default = True)
+    objects = models.Manager()
+    public = ActiveLinkManager()
+
---
+++ links/views.py
@@ -0,0 +1,43 @@
+import datetime
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.utils import timezone
+from . import models
+from django.shortcuts import render
+from rest_framework import viewsets
+
+# Create your views here.
+
+
+class LinkListApi(ListApi):
+from . import serializers
+
+
+class ActiveLinkView(APIView):
+    """
+    Returns a list of all active (publicly accessible) links
+    """
+
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        qs = models.Link.public.all()
+        data = serializers.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class RecentLinkView(APIView):
+    """
+    Returns a list of recently created active links
+    """
+
+    def get(self, request):
+        """ 
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        qs = models.Link.public.filter(created_date__gte=seven_days_ago)
+        data = serializers.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
